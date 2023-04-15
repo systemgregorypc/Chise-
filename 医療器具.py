5b63c9f2ba07eb6116584e3469cb19b6ba12d61f
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2023-04-15 12:31:03 -0400

    Update 医療器具.py

--- 医療器具.py
+++ 医療器具.py
@@ -1,1 +1,29 @@
 医療器具
+
+import math
+
+
+class Solver:
+
+    def chise(self, a, b, c):
+        d = b ** 2 - 4 * a * c
+        if d > 0:
+            disc = math.sqrt(d)
+            root1 = (-b + disc) / (2 * a)
+            root2 = (-b - disc) / (2 * a)
+            return root1, root2
+        elif d == 0:
+            return -b / (2 * a)
+        else:
+            return "この方程式には根がありません"
+
+
+if __name__ == '__main__':
+    solver = Solver()
+
+    while True:
+        a = int(input("a: "))
+        b = int(input("b: "))
+        c = int(input("c: "))
+        result = solver.chise(a, b, c)
+        print(result)
